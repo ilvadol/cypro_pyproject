ffde34d737d65023a5d18ed68d28edd86a508ea8
ilvadol <ilvadol@yandex.ru>
2025-08-03 17:35:12 +0300

    Update analysis_plotting.py

--- analysis_plotting.py
+++ analysis_plotting.py
@@ -4,7 +4,7 @@ from loading_cleaning import import_and_clean_data
 
 FIGURE_FOLDER = 'cypro_pyproject/figures/'
 
-def print_genres_by_top_publishers(df, year, num = 5, plot = True, show_plot = False):
+def genres_by_top_publishers(df, year, num = 5, plot = True, show_plot = False):
     # найти топ n издательств по продажам за определенный год
     # показать число игр, опубликованных топовыми издательствами, по жанрам за тот же год
     # эта функция показывыет на основе данных за определенный год жанры которые выпускают самые успешные по продажам компании
@@ -16,14 +16,19 @@ def print_genres_by_top_publishers(df, year, num = 5, plot = True, show_plot = F
     print("\n", ds_top_genres.value_counts())
     # print(ds_publishers.value_counts()) если хотим посмотреть топ издательств относительно кол-ва игр
     if plot:
-        ds_top_publishers.plot(kind='bar')
+        ds_top_publishers.plot(kind='bar', color='red')
+        plt.title('Top Publishers of ' + str(year))
+        plt.xlabel(None)
+        plt.ylabel(str(year) + ' Sales (in millions)')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
         FIGURE_PATH = FIGURE_FOLDER + str(year) + '_top_publishers.png'
         plt.savefig(FIGURE_PATH)
         if show_plot:
             plt.show()
 
 
-def print_total_sales_over_time_in_region(df, region, plot = True, show_plot = False):
+def total_sales_over_time_in_region(df, region, plot = True, show_plot = False):
     # Total Global Sales Over Time (Общие мировые продажи по времени)
     # → Что показывает: Общий объём продаж всех игр в мире в разбивке по месяцам или годам.
     # → Зачем: Позволяет понять общие тренды на рынке (спады, пики интереса к играм).
@@ -67,6 +72,6 @@ if __name__ == '__main__':
     DATAPATH = "cypro_pyproject/dataset/PS4_GamesSales.csv"
     verbose = False
     df = import_and_clean_data(DATAPATH, verbose)
-    print_genres_by_top_publishers(df, 2016)
-    print_total_sales_over_time_in_region(df, 'Europe')
+    genres_by_top_publishers(df, 2016)
+    total_sales_over_time_in_region(df, 'Europe')
     franchise_sales_over_time(df, 'Call of Duty', 'Global')
